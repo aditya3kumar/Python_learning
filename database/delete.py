1de1142bbf1e81ba31d8484ccf9d0c2be2baa54a
aditya3kumar <adi.aditya90@gmail.com>
2020-07-09 19:12:16 +0530

    databse code

--- database/delete.py
+++ database/delete.py
@@ -5,7 +5,6 @@ def db_delete(db):
     cursor=db.cursor()
     delete_tbl="delete from tbl_employee1 where Status= 'Inactive';"
     delete_val= cursor.execute(delete_tbl)
-    print(delete_val)
     if delete_val >0:
         print("Total Record deleted:",delete_val)
     else:
